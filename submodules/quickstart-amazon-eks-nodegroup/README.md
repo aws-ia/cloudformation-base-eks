3fb109b71fad56de6d5b491cb6b5b24c5ce27fdb
Troy Ameigh <55170274+troy-ameigh@users.noreply.github.com>
2023-05-30 22:00:08 -0400

    clean up for reset

--- submodules/quickstart-amazon-eks-nodegroup/README.md
+++
@@ -1,4 +0,0 @@
-# quickstart-amazon-eks-nodegroup
-Amazon EKS NodeGroup Quick Start
-
-This is a submodule of the [Amazon EKS QuickStart](https://github.com/aws-quickstart/quickstart-amazon-eks).
--- submodules/quickstart-eks-prometheus/build/dev.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -xe
-
-which pyenv &> /dev/null || { echo "pyenv must be installed. See https://github.com/pyenv/pyenv#installation" ; exit 1 ; }
-which pyenv-virtualenv &> /dev/null || { echo "pyenv-virtualenv must be installed. See https://github.com/pyenv/pyenv-virtualenv#installation" ; exit 1 ; }
-pyenv install 3.8.6 -s
-pyenv virtualenv 3.8.6 cloudformation-base-eks-dev || true
-eval "$(pyenv init -)"
-pyenv shell cloudformation-base-eks-dev
-pip install -qq --upgrade awscli taskcat cfn-lint git+https://github.com/aws-quickstart/qs-cfn-lint-rules.git
